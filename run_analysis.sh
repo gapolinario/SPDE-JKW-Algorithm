903ca00dbfbdd7a0084e3a1609b2d39894d8b508
gapolinario <gabrielbritoapolinario@gmail.com>
2022-05-20 18:02:44 +0200

    nonlinear

--- run_analysis.sh
+++ run_analysis.sh
@@ -6,7 +6,7 @@ numcores=8
 prefix=(0)
 
 N=( 7 8 9 10 )
-dtconst=( 1. )
+dtconst=( 0.1 )
 nu=( 0.01 )
 
 parallel -P $numcores $run '{1} {2} {3} {4}' ::: "${prefix[@]}" ::: "${N[@]}" ::: "${dtconst[@]}" ::: "${nu[@]}"
--- run_par.sh
+++ run_par.sh
@@ -11,7 +11,7 @@ ensemble_finl=$(($prefix*1000 + $size -1))
 
 R=`seq $ensemble_init $ensemble_finl`
 N=( 7 8 9 10 )
-dtconst=( 0.1 )
+dtconst=( 0.02 )
 nu=( 0.01 )
 
 parallel -P $numcores $run '{1} {2} {3} {4}' ::: "${R[@]}" ::: "${N[@]}" ::: "${dtconst[@]}" ::: "${nu[@]}"
